2f35476469d92e27e0715a3b0c2dad68ec08af44
StivenVanegas <nevits28@hotmail.com>
2021-08-19 17:35:08 -0500

    first commit

---
+++ src/test/java/edu/eci/UserMicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.UserMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
